a9ffdaf8aa6a03cb65424569b503f45bae6611a7
NotFound <vitor.s.kato@gmail.com>
2018-09-30 17:44:07 -0300

    Added monitoring app

--- app/src/main/java/android/curso/samsungchallenge/fragment/MonitoringFragment.java
+++ app/src/main/java/android/curso/samsungchallenge/fragment/MonitoringFragment.java
@@ -1,5 +1,7 @@
 package android.curso.samsungchallenge.fragment;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.curso.samsungchallenge.R;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -24,6 +26,10 @@ public class MonitoringFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.monitoring_fragment, container, false);
+
+        final Intent intentDeviceTest = new Intent("android.intent.action.MAIN");
+        intentDeviceTest.setComponent(new ComponentName("com.example.android.appsmonitor","com.example.android.monitor.ui.MainActivity"));
+        startActivity(intentDeviceTest);
         return view;
     }
 }
